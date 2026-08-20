b7551956b09a67894c92167f335664c8dcba2fc8
Gianpaulo Umberto COLETTI <info@remotebusinesspartner.com.au>
2026-08-20 16:19:30 +0800

    feat(properties): use comprehensive property workspace

--- apps/web/pages/admin/PropertyDetailWithHistoryPage.tsx
+++ apps/web/pages/admin/PropertyDetailWithHistoryPage.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import PropertyHistoryPanel from '../../components/properties/PropertyHistoryPanel';
-import PropertyDetailPage from './PropertyDetailPage';
+import PropertyWorkspacePage from './PropertyWorkspacePage';
 
-const PropertyDetailWithHistoryPage: React.FC = () => {
-  const { propertyId } = useParams<{ propertyId: string }>();
-  return (
-    <div className="space-y-6">
-      <PropertyDetailPage />
-      {propertyId && <PropertyHistoryPanel propertyId={propertyId} />}
-    </div>
-  );
-};
+const PropertyDetailWithHistoryPage: React.FC = () => <PropertyWorkspacePage />;
 
 export default PropertyDetailWithHistoryPage;
