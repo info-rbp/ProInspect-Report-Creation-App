062e35e5f51e875e314436eede8455e10a9a9eaf
Gianpaulo Umberto COLETTI <info@remotebusinesspartner.com.au>
2026-08-22 11:13:52 +0800

    fix(clients): export compatibility helpers

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -9,6 +9,7 @@ export * from './platform.js';
 export * from './propertyIntelligence.js';
 export * from './inspectionOperations.js';
 export * from './clientManagement.js';
+export * from './clientCompatibility.js';
 export * from './security.js';
 export * from './reportModel.js';
 export * from './reportOperations.js';
